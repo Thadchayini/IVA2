d89a71748d4fede610c85902a27db3ea44133990
ramanasha <ramanakarasarma@gmail.com>
2020-10-13 15:50:51 +0530

    IVA2

---
+++ js/playlist.js
@@ -0,0 +1,39 @@
+var data = {
+  "songs": [
+    {
+      "fileName": "12.mp3",
+      "service": "hello",
+      "songName": "1"
+    },
+    {
+      "fileName": "https://www.ruse-problem.org/songs/Enthusiast.mp3",
+      "service": "Birth certificate",
+      "songName": "2"
+    },
+    {
+      "fileName": "https://www.ruse-problem.org/songs/SkyRide.mp3",
+      "service": "License renewel",
+      "songName": "3"
+    },
+    {
+      "fileName": "https://www.ruse-problem.org/songs/Starling.mp3",
+      "service": "Garbage removel",
+      "songName": "4"
+    },
+    {
+      "fileName": "https://www.ruse-problem.org/songs/HachikoTheFaithtfulDog.mp3",
+      "service": "Land Permission",
+      "songName": "5"
+    },
+    {
+      "fileName": "https://www.ruse-problem.org/songs/FaterLee.mp3",
+      "service": "Companey Registration",
+      "songName": "6"
+    },
+    {
+      "fileName": "https://www.ruse-problem.org/songs/EpicSong.mp3",
+      "service": "Timber Cutting Permission",
+      "songName": "7"
+    }
+  ]
+};
